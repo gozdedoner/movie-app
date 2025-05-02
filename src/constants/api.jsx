e12b0ba09c1b27acf58de7e36119f58303ee18b0
Gozde Doner <gozde.doner.54@gmail.com>
2025-05-02 20:15:08 +0300

    Initial commit for Movie App with API integration

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,6 +1,5 @@
-export const API_KEY = "d51eef6f1f1ce96c6b0969d8dbf875e9"
-export const API_IMG = "https://image.tmdb.org/t/p/original"
-
-export const API_GENRE ="https://api.themoviedb.org/3/genre/movie/list"
-export const API_MOVIE_LIST = "https://api.themoviedb.org/3/discover/movie"
-export const API_MOVIE_DETAIL = "https://api.themoviedb.org/3/movie"
+export const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
+export const API_GENRE = import.meta.env.VITE_API_GENRE;
+export const API_MOVIE_LIST = import.meta.env.VITE_API_MOVIE_LIST;
+export const API_MOVIE_DETAIL = import.meta.env.VITE_API_MOVIE_DETAIL;
+export const API_IMG = "https://image.tmdb.org/t/p/original";
